0382b5c2d853d62daa90638881f13502c5617319
Diego Quinteros <ql17001@ues.edu.sv>
2023-11-06 17:03:26 -0600

    ✨ Add custom UserChecker

---
+++ src/Security/UserChecker.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Security;
+
+use App\Entity\Usuario;
+use Symfony\Component\Security\Core\Exception\CustomUserMessageAccountStatusException;
+use Symfony\Component\Security\Core\User\UserCheckerInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+class UserChecker implements UserCheckerInterface
+{
+    public function checkPreAuth(UserInterface $user): void
+    {
+      if (!$user instanceof Usuario) {
+        return;
+      }
+
+      if (!$user->isActivo()) {
+        // the message passed to this exception is meant to be displayed to the user
+        throw new CustomUserMessageAccountStatusException('El usuario no esta activo.');
+      }
+    }
+
+    public function checkPostAuth(UserInterface $user): void
+    {
+      
+    }
+}
